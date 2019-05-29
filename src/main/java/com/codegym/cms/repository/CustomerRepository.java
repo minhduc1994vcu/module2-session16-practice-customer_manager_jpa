d2bd77ceef48aca4db561c7b7e88ba927ec7c410
minhduc <minhduc1994vcu@gmail.com>
2019-05-29 11:53:14 +0700

    interface CustomerRepository extend Repository, improve crud methods

--- src/main/java/com/codegym/cms/repository/CustomerRepository.java
+++ src/main/java/com/codegym/cms/repository/CustomerRepository.java
@@ -1,4 +1,15 @@
 package com.codegym.cms.repository;
 
+import com.codegym.cms.model.Customer;
+
+import java.util.List;
+
 public interface CustomerRepository extends Repository {
+    List<Customer> findAll();
+
+    Customer findById(Long id);
+
+    void save(Customer model);
+
+    void remove(Long id);
 }
