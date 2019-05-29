c9926e9f854a5daab6be460005f9c4c0925fc279
minhduc <minhduc1994vcu@gmail.com>
2019-05-29 11:52:31 +0700

    nothing in this interface

--- src/main/java/com/codegym/cms/repository/Repository.java
+++ src/main/java/com/codegym/cms/repository/Repository.java
@@ -1,13 +1,6 @@
 package com.codegym.cms.repository;
 
-import java.util.List;
 
-public interface Repository <T> {
-    List<T> findAll();
-
-    T findById(Long id);
-
-    void save(T model);
-
-    void remove(Long id);
+public interface Repository {
 }
+
