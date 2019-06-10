ef0837b211adef1c79b71a211210295dc8f1224e
minhduc <minhduc1994vcu@gmail.com>
2019-06-10 14:41:41 +0700

    add 2 method findAllByProvince and findAllByFirstName

--- src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
@@ -1,9 +1,12 @@
 package com.codegym.cms.service.impl;
 
 import com.codegym.cms.model.Customer;
+import com.codegym.cms.model.Province;
 import com.codegym.cms.repository.CustomerRepository;
 import com.codegym.cms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class CustomerServiceImpl implements CustomerService {
 
@@ -11,8 +14,9 @@ public class CustomerServiceImpl implements CustomerService {
     private CustomerRepository customerRepository;
 
     @Override
-    public Iterable<Customer> findAll() {
-        return customerRepository.findAll();
+    public Page<Customer> findAll(Pageable pageable) {
+
+        return customerRepository.findAll(pageable);
     }
 
     @Override
@@ -28,5 +32,14 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void remove(Long id) {
         customerRepository.delete(id);
+    }
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstname, pageable);
     }
 }
